58a53fa477d248f568a9713e6df4b0a9b7f8c5c4
Devina <devinardya@gmail.com>
2019-10-31 09:21:17 +0100

    minor changes on changing li tag and swapping sections

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 //change website title
+
 const title = document.querySelector("title");
 title.textContent = "Fruits & Vegetables Corp";
 
 //change header text
+
 let h1 = document.querySelector("h1");
 h1.textContent = "Fruits & Vegetables Corp";
 
 //change text on the last li tag
+
 const ul = document.querySelector("ul");
-
 let item = ul.lastElementChild;
-ul.removeChild(item);
+item.textContent = "";
 
-let liNew = document.createElement("li");
 const a = document.createElement("a");
 a.setAttribute("href", "#vegetables");
 a.textContent = "Vegetables";
-liNew.appendChild(a);
-ul.appendChild(liNew);
+ul.lastElementChild.appendChild(a);
 
 // Swapping Sections
+
 let main = document.querySelector("#main");
 let contact = document.querySelector("#contact");
 let about = document.querySelector("#about");
 
-main.insertBefore(about, main.childNodes[0]);
+main.insertBefore(about, contact);
 
 // Adding H2 tag to Section "ABOUT"
+
 about.textContent="";
 let aboutTitle = document.createElement("h2");
 aboutTitle.textContent = "About";
@@ -37,35 +39,35 @@ aboutText.textContent = "We're the best in fruits & vegetables";
 about.appendChild(aboutText);
 
 // Adding H2 tag to Section "CONTACT"
+
+let contactP = contact.firstElementChild;
 let contactTitle = document.createElement("h2");
 contactTitle.textContent = "Contact";
 contact.appendChild(contactTitle);
 
-contact.insertBefore(contactTitle, contact.childNodes[0]);
+contact.insertBefore(contactTitle, contactP);
 
 // Adding main.css to head
-let head = document.querySelector("head");
+
 let linkElement = document.createElement('link');
 linkElement.setAttribute('rel', 'stylesheet');
 linkElement.setAttribute('type', 'text/css');
 linkElement.setAttribute('href', "main.css");
 
-head.appendChild(linkElement);
+document.head.appendChild(linkElement);
 
 //Fixing td into th in thead
 
-let table = document.querySelector("table");
-let tablefirst = table.firstElementChild;
-table.removeChild(tablefirst);
+let thead = document.querySelector("thead");
+let tablefirst = thead.firstElementChild;
+thead.removeChild(tablefirst);
 
-let thead = document.createElement("thead");
+let tr = document.createElement("tr");
 let firstTh = document.createElement("th");
-firstTh.textContent = "Name"
+firstTh.textContent = "Name";
 let secondTh = document.createElement("th");
-secondTh.textContent = "Email"
+secondTh.textContent = "Email";
 
-thead.appendChild(firstTh);
-thead.appendChild(secondTh);
-table.appendChild(thead);
-
-table.insertBefore(thead, table.childNodes[0]);
+tr.appendChild(firstTh);
+tr.appendChild(secondTh);
+thead.appendChild(tr);
